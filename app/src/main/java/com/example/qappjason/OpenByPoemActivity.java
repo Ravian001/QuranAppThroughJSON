4ac2bad84576aae29bccafc454f86c3044c7551b
Hassan001 <bitf19a001@pucit.edu.pk>
2023-02-11 20:18:58 +0500

    added two new empty activities for open by surah name and open by parah name functinality

---
+++ app/src/main/java/com/example/qappjason/OpenByPoemActivity.java
@@ -0,0 +1,14 @@
+package com.example.qappjason;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenByPoemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_by_poem);
+    }
+}
---
+++ app/src/main/java/com/example/qappjason/OpenByVolumeActivity.java
@@ -0,0 +1,14 @@
+package com.example.qappjason;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenByVolumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_by_volume);
+    }
+}
